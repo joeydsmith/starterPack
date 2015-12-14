e67f80fbc2087107e6556d21d8ce20063fb309d2
joeydsmith <joey@sparkexperience.com>
2015-12-14 11:06:44 -0500

    init

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Content-Type" content="text/html;utf-8">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div id="wrapper">
